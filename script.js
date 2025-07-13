4f2d61d7a17bf04109426e5be919a23689f7adea
Arafat719 <Arafatkhan01867160064@gmail.com>
2025-07-13 18:35:35 +0600

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const parah = document.querySelectorAll('.box p');
+const boxes = document.querySelectorAll('.box');
+
+let count = 0;
+
+boxes.forEach((box, index) => {
+    box.addEventListener('click', () => {
+        count++;
+        if(count === 1 || count === 3 || count === 5 || count === 7 || count === 9) {
+            parah[index].innerText = 'O';
+            parah[index].style.color = 'green';
+            console.log(index);
+        }else if(count === 2 || count === 4 || count === 6 || count === 8) {
+            parah[index].innerText = 'X';
+            parah[index].style.color = 'blue';
+            console.log(index);
+        }
+    })
+})

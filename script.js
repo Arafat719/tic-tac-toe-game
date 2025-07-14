bb68eb732d0e7c6f3ded03b870c5532964a97574
Arafat719 <Arafatkhan01867160064@gmail.com>
2025-07-14 10:12:56 +0600

    ar ekta

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const parah = document.querySelectorAll('.box p');
 const boxes = document.querySelectorAll('.box');
 
+
 const button = document.querySelector('button');
 
+const winner = document.querySelector('.winner p');
+const winnerBox = document.querySelector('.winner');
+function updateText(newText) {
+    const div = document.getElementById('myDiv');
+    div.innerText = newText;
+    // Force reflow to recalculate size
+    div.style.width = "auto"; // auto width to fit content
+}
 let count = 0;
+
 
 boxes.forEach((box, index) => {
     box.addEventListener('click', () => {
+        if (parah[index].innerText !== '') {
+            return;
+        }
+
         count++;
         if(count === 1 || count === 3 || count === 5 || count === 7 || count === 9) {
             parah[index].innerText = 'O';
@@ -17,9 +31,26 @@ boxes.forEach((box, index) => {
             parah[index].style.color = 'blue';
             console.log(index);
         }
-    })
-})
+        if (parah[0].innerText === parah[1].innerText && parah[1].innerText === parah[2].innerText && parah[0].innerText !== '') {
+            updateText();
+        } else if (parah[3].innerText === parah[4].innerText && parah[4].innerText === parah[5].innerText && parah[3].innerText !== '') {
+            updateText();
+        } else if (parah[6].innerText === parah[7].innerText && parah[7].innerText === parah[8].innerText && parah[6].innerText !== '') {
+            updateText();
+        } else if (parah[0].innerText === parah[3].innerText && parah[3].innerText === parah[6].innerText && parah[0].innerText !== '') {
+            updateText();
+        } else if (parah[1].innerText === parah[4].innerText && parah[4].innerText === parah[7].innerText && parah[1].innerText !== '') {
+            updateText();
+        } else if (parah[2].innerText === parah[5].innerText && parah[5].innerText === parah[8].innerText && parah[2].innerText !== '') {
+            updateText();
+        } else if (parah[0].innerText === parah[4].innerText && parah[4].innerText === parah[8].innerText && parah[0].innerText !== '') {
+            updateText();
+        } else if (parah[2].innerText === parah[4].innerText && parah[4].innerText === parah[6].innerText && parah[2].innerText !== '') {
+            updateText();
+        }
+    });
+});
+
 button.addEventListener('click', () => {
     location.reload();
 })
-
